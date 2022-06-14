df7f0afa98c13017ef1dc1cd3c3fd469b6e826d0
lwd-temp <26019675+lwd-temp@users.noreply.github.com>
2022-06-14 22:39:37 +0800

    bug fix

--- visitorLogger.js
+++ visitorLogger.js
@@ -16,6 +16,49 @@ function generateUUID() {
     });
 }
 
+/**
+ * 获取当前 URL 二级域名
+ * 如果当前是 IP 地址，则直接返回 IP Address
+ * https://www.jianshu.com/p/c96b0993b22e
+ */
+function getSubdomain() {
+    try {
+        let subdomain = ''
+        const key = `mh_${Math.random()}`
+        const expiredDate = new Date(0)
+        const { domain } = document
+        const domainList = domain.split('.')
+
+        const reg = new RegExp(`(^|;)\\s*${key}=12345`)
+        const ipAddressReg = /^(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])$/
+
+        // 若为 IP 地址、localhost，则直接返回
+        if (ipAddressReg.test(domain) || domain === 'localhost') {
+            return domain
+        }
+
+        const urlItems = []
+        urlItems.unshift(domainList.pop())
+
+        while (domainList.length) {
+            urlItems.unshift(domainList.pop())
+            subdomain = urlItems.join('.')
+
+            const cookie = `${key}=12345;domain=.${subdomain}`
+            document.cookie = cookie
+
+            if (reg.test(document.cookie)) {
+                document.cookie = `${cookie};expires=${expiredDate}`
+                break
+            }
+        }
+
+        return subdomain || document.domain
+    } catch (e) {
+        return document.domain
+    }
+}
+
 // Get the visitor ID from the cookie
 // Try to use the cookie if it exists, otherwise generate a new one
 // This cookie is available in any subdomain
@@ -33,8 +76,7 @@ function getVisitorID() {
     }
     if (visitorID == "") {
         visitorID = generateUUID();
-        // Get Second Level Domain
-        var domain = window.location.hostname;
+        var domain = getSubdomain();
         document.cookie = cookieName + "=" + visitorID + "; expires=Fri, 31 Dec 9999 23:59:59 GMT; domain=" + domain + ";";
     }
     return visitorID;
